350be00e2d53faae78ae52dfcb98db384b6afff3
malopezmup <36400943+malopezmup@users.noreply.github.com>
2018-03-05 20:18:30 +0100

    quiz

---
+++ cmds.js
@@ -0,0 +1,180 @@
+const model = require('./model');
+const {log, biglog, errorlog, colorize} = require("./out");
+
+exports.helpCmd = rl =>{
+	console.log("Comandos");
+	console.log(" h|help - Muestra esta yuda.");
+	console.log(" list - lista los quizes existentes.");
+	console.log(" show <id> - Muestra la pregunta y su respuesta en el quiz");
+	console.log(" add - Añadir nuevo quiz.");
+	console.log(" delete <id> - Borra el quiz indicado");
+	console.log(" edit <id> - Edita el quiz indicado");
+	console.log(" test <id> - Prueba el quiz indicado");
+	console.log(" p|play - Jugar con preguntas aleatorias de todos los quizes");
+	console.log(" credits - Créditos");
+	console.log(" q|quit - Salir del programa");
+	rl.prompt();
+};
+
+exports.quitCmd = rl =>{
+	rl.close();
+	rl.prompt();
+};
+
+exports.addCmd = rl =>{
+	rl.question(colorize(' Introduzca una pregunta: ', 'red'), question =>{
+
+			rl.question(colorize(' Introduzca la respuesta ', 'red'), answer=> {
+
+				model.add(question, answer);
+				log(` ${colorize('se ha añadido', 'magenta')}: ${question} ${colorize('=>', 'magenta')} ${answer}`),
+				rl.prompt();
+			});
+		});
+};
+
+exports.listCmd = rl =>{
+
+	model.getAll().forEach((quiz, id) =>{
+
+		log(` [${colorize(id, 'magenta')}]: ${quiz.question} `);
+	});
+	rl.prompt();
+};
+
+exports.showCmd = (rl, id) => {
+	
+	if (typeof id === "undefined"){
+		errorlog(`Falta el parametro id.`);
+	} else {
+		try{
+			const quiz = model.getByIndex(id);
+			log(` [${colorize(id, 'magenta')}]: ${quiz.question} ${colorize('=>', 'magenta')} ${quiz.answer}`);
+		} catch(error){
+			errorlog(error.message);
+		}
+	}
+
+rl.prompt();
+};
+
+exports.testCmd = (rl,id) =>{
+	if (typeof id === "undefined"){
+		errorlog(`Falta el parametro id.`);
+		rl.prompt();
+	} else {
+		try{
+			const quiz = model.getByIndex(id);
+			rl.question(`${quiz.question}: `, answer =>{
+				if(answer.toLowerCase().trim() === quiz.answer.toLowerCase().trim()){
+					biglog(' Correcto', 'green');
+				}
+				else{
+					biglog(' Incorrecto', 'red');
+				}	
+			});
+			rl.prompt();
+		} catch(error){
+			errorlog(error.message);
+			rl.prompt();
+		}
+	}
+};
+exports.deleteCmd = (rl,id) =>{
+	if (typeof id === "undefined"){
+		errorlog(`Falta el parametro id.`);
+	} else {
+		try{
+			model.delete(id);
+			
+		} catch(error){
+			errorlog(error.message);
+		}
+	}
+
+rl.prompt();
+};
+	
+
+exports.playCmd = rl => {
+	let score = 0;
+	pendientes = [];
+	let long= model.count();
+	for (i =0; i<model.count(); i++) {
+		pendientes.push(i);
+	}
+	function numeroAleatorio(min, max) {
+ 			 return Math.round(Math.random() * (max - min) + min);
+			}
+	const askrandom = () => {
+
+		if (pendientes.length===0){
+			log("Ha completado el quiz");
+			log(`Ha obtenido una puntuación de: ${colorize(score, 'green')} sobre ${colorize(long, 'green')}`);
+			rl.prompt();
+		}else{
+					let randi = numeroAleatorio(0,pendientes.length-1);
+					let id = pendientes[randi];
+					pendientes.splice(randi,1);
+			
+				try{
+					const quiz=model.getByIndex(id);
+					rl.question(`${quiz.question}: `, answer =>{
+						if(answer.toLowerCase().trim() === quiz.answer.toLowerCase().trim()){
+							biglog(' Correcto', 'green');
+							score++;
+							log(`Ha obtenido una puntuación de: ${colorize(score, 'green')} hasta ahora`);
+							askrandom();
+						}else{
+							biglog(' Incorrecto', 'red');
+							log("Fin del juego, más suerte la próxima");
+							log(`Ha obtenido una puntuación de: ${colorize(score, 'green')} sobre ${colorize(long, 'green')}`);
+							rl.prompt();
+							}	
+						});
+
+				}catch(error){
+					errorlog(error.message);
+					rl.prompt();
+				}
+		}
+
+	}
+	askrandom();
+};
+
+exports.editCmd = (rl,id) =>{
+	if (typeof id === "undefined"){
+		errorlog(`Falta el parametro id.`);
+		rl.prompt();
+	} else {
+		try{
+			const quiz = model.getByIndex(id);
+
+			process.stdout.isTTY && setTimeout (() => {rl.write(quiz.question)},0);
+
+			rl.question(colorize(' Introduzca una pregunta: ', 'red'), question =>{
+
+				process.stdout.isTTY && setTimeout (() => {rl.write(quiz.answer)},0);
+				rl.question(colorize(' Introduzca la respuesta: ', 'red'), answer=> {
+					model.update(id, question, answer);
+					log(` Se ha cambiado el quiz ${colorize(id, 'magenta')} por: ${question} ${colorize('=>', 'magenta')} ${answer}`)
+				});
+			});
+			
+			rl.prompt();
+		} catch(error){
+			errorlog(error.message);
+			rl.prompt();
+		}
+	}
+
+
+};
+
+
+exports.creditsCmd = rl =>{
+	console.log('Autor de la practica');
+	console.log('Miguel Angel Lopez Muñoz');
+	rl.prompt();
+};
---
+++ model.js
@@ -0,0 +1,104 @@
+//preguntas
+const fs = require("fs");
+const DB_FILENAME = "quizes.json"
+
+let quizes = [
+{
+	question:"Capital de Malasia",
+	answer:"Kuala Lumpur"
+},
+{
+	question:"Capital de Mozambique",
+	answer:"Maputo"
+},
+{
+	question:"Capital de España",
+	answer:"Madrid"
+},
+{
+	question:"Capital de Noruega",
+	answer:"Oslo"
+}];
+
+
+const load = () =>{
+
+	fs.readFile(DB_FILENAME, (err, data) => {
+  if (err) {
+    if (err.code === 'ENOENT') {
+      save();
+      return;
+    }
+
+    throw err;
+  }
+  	let json = JSON.parse(data);
+
+  	if(json){
+  		quizes = json;
+  	}
+	});
+};
+
+//guardar
+const save = () =>{
+
+	fs.writeFile(DB_FILENAME, JSON.stringify(quizes),
+		err => {
+			if (err) throw err;
+		});
+};
+
+
+
+
+//contar elemntos
+exports.count = () => quizes.length;
+
+//añadir
+exports.add = (question, answer) =>{
+
+	quizes.push({
+		question: (question || "").trim(),
+		answer: (answer || "").trim()
+
+	});
+	save();
+};
+
+exports.update = (id, question, answer) => {
+
+	const quiz = quizes[id];
+	if (typeof quiz === "undefined"){
+		throw new Error(`El valor de id no es valido,`);
+	}
+	quizes.splice(id, 1 ,{
+		question: (question || "").trim(),
+		answer: (answer || "").trim()
+	});
+	save();
+};
+//devuelve todos los quizes
+exports.getAll = () => JSON.parse(JSON.stringify(quizes));
+
+//dame el quiz de id que se ponga como parametro
+exports.getByIndex = id => {
+	const quiz = quizes[id];
+	if (typeof quiz === "undefined"){
+		throw new Error(`El valor del parametro id no es valido,`);
+	}
+	return JSON.parse(JSON.stringify(quiz));
+}; 
+
+//Borrar el quiz de id seleccionado
+exports.delete = id => {
+	const quiz = quizes[id];
+	if (typeof quiz === "undefined"){
+		throw new Error(`El valor de id no es valido,`);
+	}
+	quizes.splice(id, 1);
+	save();
+};
+
+
+load();
